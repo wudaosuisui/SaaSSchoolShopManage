cfe059ebeee3e46ef2a23bcbbb412b6659a8ff46
wudaosuisui <1450403471@qq.com>
2019-05-05 23:24:32 +0800

    SaleGoods  @Entity 商品销售表

---
+++ SaaS-schoolShop-manage/src/com/saasschoolshopmanage/stor/log/SaleGoods.java
@@ -0,0 +1,111 @@
+package com.saasschoolshopmanage.stor.log;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class SaleGoods {
+
+	private int id;
+	private int shopId;
+	private int adminId;
+	private String adminName;
+	private String goodsName;
+	private int count;
+	private int price;
+	private int couPrice;
+	private String img;
+	private Date timeStamp;
+
+	public SaleGoods() {
+	}
+
+	@Id
+	@GeneratedValue(generator = "my_gen")
+	@GenericGenerator(name = "my_gen", strategy = "increment")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(int shopId) {
+		this.shopId = shopId;
+	}
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCouPrice() {
+		return couPrice;
+	}
+
+	public void setCouPrice(int couPrice) {
+		this.couPrice = couPrice;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+}
