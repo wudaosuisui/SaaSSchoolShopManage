d23817d14bd1c18827b500e3669974774c079e04
wudaosuisui <1450403471@qq.com>
2019-05-05 22:27:33 +0800

    StudentStarActivity @Entity 学生关注活动表

---
+++ SaaS-schoolShop-manage/src/com/saasschoolshopmanage/stor/entity/StudentStarActivity.java
@@ -0,0 +1,46 @@
+package com.saasschoolshopmanage.stor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class StudentStarActivity {
+
+	private int id;
+	private int stuId;
+	private int actId;
+
+	public StudentStarActivity() {
+	}
+
+	@Id
+	@GeneratedValue(generator = "my_gen")
+	@GenericGenerator(name = "my_gen", strategy = "increment")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStuId() {
+		return stuId;
+	}
+
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+
+	public int getActId() {
+		return actId;
+	}
+
+	public void setActId(int actId) {
+		this.actId = actId;
+	}
+
+}
