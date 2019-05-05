3cb863150b852293b16f8838484ab6e3c59d7d91
wudaosuisui <1450403471@qq.com>
2019-05-05 22:30:11 +0800

    StudentStarShop  @Entity 学生关注店铺表

---
+++ SaaS-schoolShop-manage/src/com/saasschoolshopmanage/stor/entity/StudentStarShop.java
@@ -0,0 +1,44 @@
+package com.saasschoolshopmanage.stor.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+public class StudentStarShop {
+
+	private int id;
+	private int stuId;
+	private int shopId;
+
+	public StudentStarShop() {
+	}
+
+	@Id
+	@GeneratedValue(generator = "my_gen")
+	@GenericGenerator(name = "my_gen", strategy = "increment")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStuId() {
+		return stuId;
+	}
+
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+
+	public int getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(int shopId) {
+		this.shopId = shopId;
+	}
+
+}
